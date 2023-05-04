50c78dabadec7e40844d11747fa67216c4d6e863
exam <exam@Bcalab4-m03>
2023-05-04 09:37:50 +0530

    Added goforward method

--- src/Book_203.java
+++ src/Book_203.java
@@ -7,6 +7,10 @@ public class Book_203 {
 		this.total_pages=total_pages;
 		this.page_num=1;
 	}
+	int go_forward() {
+		page_num++;
+		return page_num;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
